83d872da0ed163cac9a52d9d9df4e9e2d6266793
mindaugasinas <sinkeviciusm@gmail.com>
2018-02-27 12:46:12 +0200

    Su intro graphics

--- core/src/com/mygdx/game/MainMenu.java
+++ core/src/com/mygdx/game/MainMenu.java
@@ -18,11 +18,16 @@ public class MainMenu extends AbstractScreen {
         super(game);
     }
 
+
+
     @Override
     public void show () {
-        title = new TextureRegion(new Texture(Gdx.files.internal("core\\assets\\title.png")), 0, 0, 480, 320);
+        title = new TextureRegion(new Texture(Gdx.files.internal("core/assets/platGameIntro.png")), 0, 0, 480, 320);
         batch = new SpriteBatch();
         batch.getProjectionMatrix().setToOrtho2D(0, 0, 480, 320);
+
+
+
     }
 
     @Override
